38312a075a20e6ab4db053cc5141c5b090a5085e
Xerxes <5236639+xerxes-at@users.noreply.github.com>
2020-08-16 20:11:21 +0200

    Updated Bat file for new Update.

--- !start_zm_server.bat
+++ !start_zm_server.bat
@@ -1,17 +1,23 @@
 @echo off
-::name of the config file the server should use.
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=placeServerKeyHere
+::Name of the config file the server should use. (default: dedicated_zm.cfg)
 set cfg=dedicated_zm.cfg
-::name of the server shown in the title of the cmd window
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
 set name=ZM 1
-::Port used by the server
+::Port used by the server (default: 4976)
 set port=4976
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
 
 title PlutoniumT6 - %name% - Server restarter
 echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
 echo Server "%name%" will load %cfg% and listen on port %port% UDP!
 echo To shut down the server close this window first!
 echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
 :server
-start /wait /abovenormal t6rzm.exe -dedicated +sv_config %cfg% +net_port %port%
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t6zm "%gamepath%" -dedicated +set key %key% +sv_config %cfg% +net_port %port%
 echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto server
