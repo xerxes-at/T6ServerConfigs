980cc224935ba28a22cfdd305a02ce3a8973acca
Xerxes <5236639+xerxes-at@users.noreply.github.com>
2024-08-21 19:59:52 +0200

    Don't forget to push the zm batch file

--- !start_zm_server.bat
+++ !start_zm_server.bat
@@ -20,6 +20,6 @@ echo (%date%)  -  (%time%) %name% server start.
 
 cd /D %LOCALAPPDATA%\Plutonium
 :server
-start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t6zm "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +set net_port %port% +exec %cfg%
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t6zm "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +set net_port %port% +exec %cfg% +map_rotate
 echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto server
